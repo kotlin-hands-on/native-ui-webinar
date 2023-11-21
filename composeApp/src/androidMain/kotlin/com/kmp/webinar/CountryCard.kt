2c78b3db5a148a8a0d69322f1b9f459511937c34
pahill <pamela.a.hill@gmail.com>
2023-11-21 13:55:45 +0200

    Align UI with material

--- composeApp/src/androidMain/kotlin/com/kmp/webinar/CountryCard.kt
+++ composeApp/src/androidMain/kotlin/com/kmp/webinar/CountryCard.kt
@@ -1,24 +1,25 @@
 package com.kmp.webinar
 
 import androidx.compose.foundation.border
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.material.Card
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import country.Country
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.unit.dp
+import country.Country
 
 @Composable
-fun CountryCard(modifier: Modifier, country: Country, currentCountry: Boolean, navigateToWeather: (String, Double, Double)-> Unit) {
-    Card(
-        modifier = modifier.fillMaxWidth().padding(4.dp),
-        elevation = 10.dp,
-        shape = MaterialTheme.shapes.medium,
-        ) {
+fun CountryCard(
+    modifier: Modifier,
+    country: Country,
+    currentCountry: Boolean,
+    navigateToWeather: (String, Double, Double) -> Unit
+) {
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .padding(4.dp)) {
         if (currentCountry) {
             Box(modifier = Modifier.border(5.dp, Color.DarkGray)) {
                 Country(Modifier, country, navigateToWeather)
--- composeApp/src/androidMain/kotlin/com/kmp/webinar/WeatherButton.kt
+++ composeApp/src/androidMain/kotlin/com/kmp/webinar/WeatherButton.kt
@@ -2,34 +2,43 @@ package com.kmp.webinar
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import country.CapitalInfo
 
 @Composable
-fun WeatherButton(modifier: Modifier = Modifier, capitals: List<String>, capitalInfo: CapitalInfo, navigateToWeather: (String, Double, Double) -> Unit) {
-    val navController = rememberNavController()
+fun WeatherButton(
+    modifier: Modifier = Modifier,
+    capitals: List<String>,
+    capitalInfo: CapitalInfo,
+    navigateToWeather: (String, Double, Double) -> Unit
+) {
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.Bottom
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             capitals.forEach {
-                Button(
-                    shape = RoundedCornerShape(20.dp),
-                    colors = ButtonDefaults.outlinedButtonColors(
+                TextButton(
+                    colors = ButtonDefaults.textButtonColors(
                         contentColor = Color.Black
                     ),
                     onClick = {
-                        navigateToWeather(it, capitalInfo.latlng[0].toDouble(), capitalInfo.latlng[1].toDouble())
+                        navigateToWeather(
+                            it,
+                            capitalInfo.latlng[0],
+                            capitalInfo.latlng[1]
+                        )
                     }) {
                     Text(text = "$it weather")
                 }
